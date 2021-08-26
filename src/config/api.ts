620071b9e5720df28b2ae5f7140334c6d3820dcb
koyaaoyama <aoyama.ko0920@gmail.com>
2021-08-26 22:28:15 +0900

    add global constant file

---
+++ src/config/api.ts
@@ -0,0 +1,7 @@
+/**
+ * common constants about TMDB API
+ */
+export default Object.freeze({
+	//ex.) <img src={`${config.IMG_BASE_PATH}/xxx/yyy.jpg`} />
+	IMG_BASE_PATH: "https://image.tmdb.org/t/p/original"
+});
