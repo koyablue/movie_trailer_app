9628cf60fccc7530fdc87a1dbfe003a60b879afc
koyaaoyama <aoyama.ko0920@gmail.com>
2021-08-29 00:20:10 +0900

    fix App component. access movie genre endpoint to show Row component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,42 @@
-import React from "react";
-import "./App.css";
+import React, { useEffect, useState } from "react";
 import { Row } from "./components/Row";
 import{ Banner } from "./components/Banner";
-import { requests } from "../src/request";
+import { Nav } from "./components/Nav";
+import { endpoints } from "./api_endpoints";
+import axiosInstance from "./axios";
+import "./stylesheets/App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <Banner />
-      <Row
-        title="NETFLIX ORIGINALS"
-        fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
-      />
-    </div>
-  );
-}
+interface MovieGenre {
+	id: number;
+	name: string;
+};
+
+const App = () => {
+	const [movieGenres, setMovieGenres] = useState<MovieGenre[]>([]);
+
+	useEffect(() => {
+		const fetchMovieGenres = async () => {
+			const idsAndNamesOfGenres = await axiosInstance.get(endpoints.fetchMovieGenres);
+			setMovieGenres(idsAndNamesOfGenres.data.genres);
+		};
+		fetchMovieGenres();
+	}, []);
+
+	return (
+		<div className="App">
+			<Nav />
+			<Banner />
+			{/* <Row
+			title="NETFLIX ORIGINALS"
+			fetchUrl={requests.fetchNetflixOriginals}
+			isLargeRow
+			/> */}
+			{movieGenres.map(idAndName =>
+				<Row title={idAndName.name} fetchUrl={`${endpoints.fetchMoviesByGenreId}${idAndName.id}`} />
+			)}
+
+		</div>
+	);
+};
 
 export default App;
